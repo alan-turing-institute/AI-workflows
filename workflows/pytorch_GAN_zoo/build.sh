26e569e285b27c0884e2a54fa4b85c2de980fa17
Jim Madge <jim@jmadge.com>
2021-07-29 08:34:13 +0100

    Change name of UID environment variable
    On linux UID is already set and is read only.

--- workflows/pytorch_GAN_zoo/build.sh
+++ workflows/pytorch_GAN_zoo/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-UID=$(id -u)
+_UID=$(id -u)
 DEF_FILE="pytorch_GAN_zoo.def"
 SIF_FILE="pytorch_GAN_zoo.sif"
 
-if [ "$UID" = 0 ]; then
+if [ "$_UID" = 0 ]; then
     singularity build $SIF_FILE $DEF_FILE
 else
     singularity build --fakeroot $SIF_FILE $DEF_FILE
