75a64bdaef16c4be03c4ebb966813c8b0763d5ac
dead-water <toxicgas1@mac.com>
2022-05-10 14:54:26 +0100

    fixed generation code to use correct env vars for paths

--- workflows/jamcam/jamcam/entry.sh
+++ workflows/jamcam/jamcam/entry.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 # Entrypoint for jamcam workflow, requires execution within suitable container. See README
 
+# %environment
+# export USER_EXPERIMENT_DIR=/workspace/yolo_v4
+# export DATA_DOWNLOAD_DIR=/workspace/data
+# export SPECS_DIR=/workspace/yolo_v4/specs
+# export LOCAL_SPECS_DIR=/workspace/yolo_v4/specs
+# export LOCAL_PROJECT_DIR=/workspace
+
 while [ -n "$1" ]; do # 
 	case "$1" in
 	--verify) 		echo "Verifying Environment and data...\n" &&
 					python3 jamcam/verify.py ;; # env & dataset verification
 	
 	--gen-valid) 	echo "Generating validation dataset...\n" &&
-					python3 ../ssd/generate_val_dataset.py 
-								--input_image_dir=$LOCAL_DATA_DIR/train/images \
-                                --input_label_dir=$LOCAL_DATA_DIR/train/labels \
-                                --output_dir=$LOCAL_DATA_DIR/val ;; 
+					python3 jamcam/generate_val_dataset.py --input_image_dir=$DATA_DOWNLOAD_DIR/train/images --input_label_dir=$DATA_DOWNLOAD_DIR/train/labels --output_dir=$DATA_DOWNLOAD_DIR/val ;; 
 	
 	--tune-bdb) echo "Tuning bounding box...\n" ;;
 	--convert-data) echo "Converting dataset formats...\n" ;;
+		# this is actually already done in the verification, could be split further
 	--train) echo "Beginning training with default parameters...\n" ;;
+
 	--eval) echo "Beginning evaluation with default parameters...\n" ;;
+
 	--vis) echo "Displaying visualisation...\n" ;;
+
 	--export) echo "Exporting model...\n" ;;
 
 	*) echo "Option $1 not recognized" ;;
