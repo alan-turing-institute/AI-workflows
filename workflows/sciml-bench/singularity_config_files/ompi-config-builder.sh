c82b02d5df485ab85bbee2a0ae43a454db41387a
Luke Hare <lhare@turing.ac.uk>
2022-05-24 15:46:50 +0100

    Clean up sciml-bench workflow and add cuda 11 def files

--- workflows/sciml-bench/singularity_config_files/ompi-config-builder.sh
+++
@@ -1,3 +0,0 @@
-{ (envsubst < ompi-header.txt) & (cat ompi-body.txt); } > tmp_file.def 2>/dev/null
-sudo -E singularity build ${OMPI4_CONTAINER} tmp_file.def
-rm tmp_file.def
--- workflows/sciml-bench/singularity_config_files/sciml-config-builder.sh
+++
@@ -1,3 +0,0 @@
-{ (envsubst < sciml-config-header.txt) & (cat sciml-config-body.txt); } > tmp_file.def 2>/dev/null
-sudo -E singularity build sciml-bench-with-config.sif tmp_file.def
-rm tmp_file.def
