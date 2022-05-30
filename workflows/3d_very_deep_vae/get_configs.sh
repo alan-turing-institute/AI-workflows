2db18d6bda5897369965926a32aceea10b2e7806
Jim Madge <jim@jmadge.com>
2022-05-30 14:00:10 +0100

    Add script to write example configurations

---
+++ workflows/3d_very_deep_vae/get_configs.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+cp /3d_very_deep_vae/example_configurations/VeryDeepVAE_32x32x32.json ./
+cp /3d_very_deep_vae/example_configurations/VeryDeepVAE_64x64x64.json ./
+cp /3d_very_deep_vae/example_configurations/VeryDeepVAE_128x128x128.json ./
