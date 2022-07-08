6a85ce7c0beca13e56d9b6d223970ca5e1e899c9
Luke Hare <lhare@turing.ac.uk>
2022-07-08 09:56:58 +0100

    Remove need for storage_path from get_data.sh

--- workflows/sciml-bench/get_data.sh
+++ workflows/sciml-bench/get_data.sh
@@ -19,9 +19,9 @@ case ${BENCHMARK_NAME} in
     exit 1;;
 esac
 
-mkdir -p "${STORAGE_PATH}${SLURM_JOB_NAME}"
+mkdir -p "${SLURM_JOB_NAME}"
 
-if [[ ! -d ${STORAGE_PATH}"datasets/${DATASET_NAME}" ]]
+if [[ ! -d "datasets/${DATASET_NAME}" ]]
 then
-	singularity run --nv ${STORAGE_PATH}${CONTAINER} download ${DATASET_NAME} --dataset_root_dir=${STORAGE_PATH}"datasets/"
+	singularity run --nv ${CONTAINER} download ${DATASET_NAME} --dataset_root_dir="datasets/"
 fi
